77d7e8d4e53c7a7bcb49bbd0efeaeeab3472d274
Ashish Garg <mragarg@gmail.com>
2019-05-10 14:24:03 -0400

    Skeleton for Components to portfolio page

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class AboutMe extends React.Component {
+    render() {
+        return (
+        <div>
+            <h1>About Page</h1>
+        </div>
+        )
+    };
+}
+
+export default AboutMe;
