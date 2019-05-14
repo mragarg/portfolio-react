e177162063ac629fdf5a396a6d80bf5be24458c5
Ashish Garg <mragarg@gmail.com>
2019-05-14 11:29:15 -0400

    Adds function to toggle categories for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 export class Projects extends React.Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         activeTab: 0
+      }
+    }
+
+    _toggleCategories = () => {
+        if(this.state.activeTab === 0){
+            return(
+                <div>
+                   <h1>This is React</h1>
+                </div>
+            );
+        }
+    }
+
+
     render() {
         return (
-        <div>
-            <h1>Projects Page</h1>
+        <div className="category-tabs">
+            <Tabs activeTab={this.state.activeTab} onChange={(tabId) => {
+                this.setState({
+                    activeTab: tabId
+                });
+            }} ripple>
+                <Tab>Projects</Tab>
+            </Tabs>
+
+            <section className="projects-grid">
+                {this._toggleCategories()}
+            </section>
         </div>
         )
     };
