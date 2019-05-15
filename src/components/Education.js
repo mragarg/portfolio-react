1b287a56356ea58c950984e82638d7f39b45298d
Ashish Garg <mragarg@gmail.com>
2019-05-14 21:04:56 -0400

    Formats

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,15 +4,15 @@ import { Grid, Cell } from 'react-mdl';
 export class Education extends React.Component {
     render() {
         return (
-        <Grid>
-            <Cell col={4}>
-                <p>{this.props.startDate} - {this.props.endDate}</p>
-            </Cell>
-            <Cell col={8}>
-                <h4 style={{marginTop: '0px'}}>{this.props.schoolName}</h4>
-                <p>{this.props.schoolDescription}</p>
-            </Cell>
-        </Grid>
+            <Grid>
+                <Cell col={4}>
+                    <p>{this.props.startDate} - {this.props.endDate}</p>
+                </Cell>
+                <Cell col={8}>
+                    <h4 style={{marginTop: '0px'}}>{this.props.schoolName}</h4>
+                    <p>{this.props.schoolDescription}</p>
+                </Cell>
+            </Grid>
         )
     }
 }
