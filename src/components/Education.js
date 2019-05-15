594f246be1da63cfc346e756705939010c5585c9
Ashish Garg <mragarg@gmail.com>
2019-05-14 20:57:21 -0400

    Format Education component

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { Grid, Cell } from 'react-mdl';
 
 export class Education extends React.Component {
-  render() {
-    return (
-      <Grid>
-          <Cell col={4}>
-            <p>{this.props.startDate} - {this.props.endDate}</p>
-          </Cell>
-          <Cell col={8}>
-            <h4 style={{marginTop: '0px'}}>{this.props.schoolName}</h4>
-            <p>{this.props.schoolDescription}</p>
-          </Cell>
-      </Grid>
-    )
-  }
+    render() {
+        return (
+        <Grid>
+            <Cell col={4}>
+                <p>{this.props.startDate} - {this.props.endDate}</p>
+            </Cell>
+            <Cell col={8}>
+                <h4 style={{marginTop: '0px'}}>{this.props.schoolName}</h4>
+                <p>{this.props.schoolDescription}</p>
+            </Cell>
+        </Grid>
+        )
+    }
 }
 
 export default Education;
